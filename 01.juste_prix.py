b98fa019859c04e5fc65f632b3f302ef0fb2bad5
jojo11h <148336324+jojo11h@users.noreply.github.com>
2023-10-22 21:20:11 +0200

    Update 01.juste_prix.py

--- 01.juste_prix.py
+++ 01.juste_prix.py
@@ -1,26 +1,43 @@
 import random as r
 
-prix = r.randint(1, 10)
-
-score = 100
-
-tentatives = 0
-
-bool = False
-
 print("Devinez le juste prix ! Le prix est un nombre compris entre 1 et 10 inclus.")
 
-while bool == False:
-    nombre = int(input('=> '))
-    tentatives += 1
-    if nombre < prix:
-        print("Le just prix est plus haut")
 
-    elif nombre > prix:
-        print("Le juste prix est plus bas")
+def choose_difficulty():
+    while True:
+        difficult = input(
+            'Choisissez la difficulté (1-facile, 2-moyen, 3-difficile) : ')
+        if difficult == '1':
+            return r.randint(1, 10)
+        elif difficult == '2':
+            return r.randint(1, 50)
+        elif difficult == '3':
+            return r.randint(1, 100)
+        else:
+            print('Veuillez entrer une difficulté valide (1, 2 ou 3)')
 
-    else:
-        print(
-            f"Félicitations, vous avez trouvé le juste prix {prix} en {tentatives} essais, votre score est {int(score / tentatives)} !")
 
-        bool = True
+def game(prix):
+    bool = False
+    score = 100
+    tentatives = 0
+
+    while not bool:
+        nombre = int(input('=> '))
+        tentatives += 1
+        if nombre < prix:
+            print("Le just prix est plus haut")
+
+        elif nombre > prix:
+            print("Le juste prix est plus bas")
+
+        else:
+            print(
+                f"Félicitations, vous avez trouvé le juste prix {prix} en {tentatives} essais, votre score est {int(score / tentatives)} !")
+
+            bool = True
+
+
+prix = choose_difficulty()
+print(prix)
+game(prix)
